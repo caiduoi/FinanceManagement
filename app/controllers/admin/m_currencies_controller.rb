6be1aab8fe36bd0739133418f2bec75f8514affd
LeMinhThanh <le.minh.thanh@framgia.com>
2014-03-21 16:49:26 +0700

    Export csv currency

--- app/controllers/admin/m_currencies_controller.rb
+++ app/controllers/admin/m_currencies_controller.rb
@@ -3,6 +3,10 @@ class Admin::MCurrenciesController < ApplicationController
   def index
     @search = M::Currency.search(params[:q])
     @currencies = @search.result
+    respond_to do |format|
+      format.html
+      format.csv { send_data @currencies.to_csv }
+    end
   end
   
 end
--- app/models/m/currency.rb
+++ app/models/m/currency.rb
@@ -1,2 +1,12 @@
 class M::Currency < ActiveRecord::Base
+  
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |currency|
+        csv << currency.attributes.values_at(*column_names)
+      end
+    end
+  end
+  
 end
